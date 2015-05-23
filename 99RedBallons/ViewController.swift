2f57ed75ac9979773092921a719f0c068ccaf89e
Tyko <kaleb91.riley@gmail.com>
2015-05-23 10:26:26 -0400

    Initial Commit

---
+++ 99RedBallons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  99RedBallons
+//
+//  Created by User on 5/23/15.
+//  Copyright (c) 2015 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
